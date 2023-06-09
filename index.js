4e1e60764cbdd9a76f832b5f9682448711be53ba
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-06-09 22:34:52 +0600

    make instructor

--- index.js
+++ index.js
@@ -69,7 +69,18 @@ async function run() {
       res.send(result)
     })
 
-    
+    // instructor patch
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role2: 'instructor'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
 
 
 
