761ce7521e3d3198487b060539b771496fe665ad
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-06-09 22:34:15 +0600

    make admin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-euh9qdo-shard-00-00.hbyxuz9.mongodb.net:27017,ac-euh9qdo-shard-00-01.hbyxuz9.mongodb.net:27017,ac-euh9qdo-shard-00-02.hbyxuz9.mongodb.net:27017/?ssl=true&replicaSet=atlas-ny4qda-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,21 +40,40 @@ async function run() {
     app.post('/users', async (req, res) => {
       const user = req.body;
       console.log(user)
-      const query={email:user?.email}
+      const query = { email: user?.email }
       console.log(query)
-      const existingUser=await usersCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exists'})
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
       }
-      const result=await usersCollection.insertOne(user)
+      const result = await usersCollection.insertOne(user)
       res.send(result)
     })
 
     // get users
-    app.get('/users', async(req, res)=>{
-      const result=await usersCollection.find().toArray();
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.send(result);
     })
+
+    // admin patch
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role1: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
