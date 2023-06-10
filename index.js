eb901b195441f916b412b79b77f5e2b3d4c57d5c
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-06-10 22:08:28 +0600

    get classes

--- index.js
+++ index.js
@@ -171,6 +171,13 @@ async function run() {
       res.send(result)
     })
 
+
+    //get classes
+    app.get('/classes', async(req, res)=>{
+      const result=await classesCollection.find().toArray();
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
